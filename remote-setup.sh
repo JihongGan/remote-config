37774106148277f1088f1bb24cd2fdd325d1690c
Jihong Gan <jhgan99@gmail.com>
2022-01-24 20:45:44 -0500

    run github cli

--- remote-setup.sh
+++ remote-setup.sh
@@ -27,6 +27,7 @@ sudo timedatectl set-timezone America/New_York
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt install gh
+gh auth login
 
 # vim setup
 # TODO: no need to manually install vim unless it's ubuntu16.04
