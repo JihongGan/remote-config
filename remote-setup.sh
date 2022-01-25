1f16432e7fa24ab832e1448d1530a362454fd995
Jihong Gan <jhgan99@gmail.com>
2022-01-24 20:31:28 -0500

    bug fix

--- remote-setup.sh
+++ remote-setup.sh
@@ -47,6 +47,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/jeffreytse/zsh-vi-mode \
   $ZSH/custom/plugins/zsh-vi-mode
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# install autojump
+cd ~
 git clone git://github.com/wting/autojump.git
 cd autojump
 ./install.py
